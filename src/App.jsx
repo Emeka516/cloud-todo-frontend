b294d9d05a731d1cb739dd909e6f733a7f93bd39
Nwaeke Victor <victornwaeke12@gmail.com>
2026-03-29 20:55:22 +0100

    update API URL to use environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { useState, useEffect } from "react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/tasks")
+    fetch(`${API_URL}/tasks`)
       .then((res) => res.json())
       .then((data) => setTasks(data))
       .catch((err) => console.error(err));
   }, []);
 
   const addTask = (task) => {
-    fetch("http://localhost:5000/tasks", {
+    fetch(`${API_URL}/tasks`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -26,7 +28,7 @@ function App() {
   };
 
   const deleteTask = (id) => {
-    fetch(`http://localhost:5000/tasks/${id}`, {
+    fetch(`${API_URL}/tasks/${id}`, {
       method: "DELETE",
     })
       .then(() => {
@@ -35,31 +37,27 @@ function App() {
       .catch((err) => console.error(err));
   };
 
-
- const editTask = (id) => {
-   const newTitle = prompt("Edit task title:");
-   if (newTitle) {
-     fetch(`http://localhost:5000/tasks/${id}`, {
-       method: "PUT",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify({ title: newTitle }),
-     })
-       .then((res) => res.json())
-       .then((updatedTask) => {
-         setTasks(
-           tasks.map((task) =>
-             task._id === updatedTask._id ? updatedTask : task,
-           ),
-         );
-       })
-       .catch((err) => console.error(err));
-   }
- };
-
- 
-
+  const editTask = (id) => {
+    const newTitle = prompt("Edit task title:");
+    if (newTitle) {
+      fetch(`${API_URL}/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: newTitle }),
+      })
+        .then((res) => res.json())
+        .then((updatedTask) => {
+          setTasks(
+            tasks.map((task) =>
+              task._id === updatedTask._id ? updatedTask : task,
+            ),
+          );
+        })
+        .catch((err) => console.error(err));
+    }
+  };
 
   return (
     <div className="App">
